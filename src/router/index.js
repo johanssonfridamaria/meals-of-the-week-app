cd9db64e4232b0a2cce7d261eb10151b9156b1b5
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-21 10:21:02 +0100

    added routes to meals and thisweeksmenu

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Meals from '../views/Meals.vue'
+import ThisWeeksMenu from '../views/ThisWeeksMenu.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'meals',
+    component: Meals
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/menu',
+    name: 'ThisWeeksMenu',
+    component: ThisWeeksMenu
+  },
 ]
 
 const router = new VueRouter({
