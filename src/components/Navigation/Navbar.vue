0ba19b40a8e45adc8dc11cb0c5469676adcf29d0
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-21 11:06:22 +0100

    added styling

--- src/components/Navigation/Navbar.vue
+++ src/components/Navigation/Navbar.vue
@@ -1,8 +1,11 @@
 <template>
-  <nav>
-    <div>
-      <router-link to="/">Meals</router-link>
-      <router-link to="/menu">This weeks menu</router-link>
+  <nav class="navbar">
+    <div class="container">
+      <router-link class="nav-link" to="/">LOGO</router-link>
+      <div class="">
+        <router-link class="nav-link" to="/">Meals</router-link>
+        <router-link class="nav-link" to="/menu">This weeks menu</router-link>
+      </div>
     </div>
   </nav>
 </template>
@@ -10,9 +13,16 @@
 <script>
 export default {
   name: 'Navbar',
-}
+};
 </script>
 
-<style>
-
+<style scoped>
+.navbar{
+  background-color: beige;
+  padding: 1rem 0;
+}
+.nav-link {
+  text-decoration: none;
+  color: #333;
+}
 </style>
