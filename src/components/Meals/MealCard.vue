fdecb98f3789ea0473f47e0a7b32b152865017e9
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-21 16:34:43 +0100

    created card for each meal, added styling

---
+++ src/components/Meals/MealCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="meal-card d-flex column">
+      <h1>{{ meal.index }}</h1>
+      <h1>{{ meal.name }}</h1>
+      <div>
+      <button class="btn">Remove</button>
+      <button class="btn">Add to "This weeks menu"</button>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MealCard',
+  props: ['meal']
+};
+</script>
+
+<style scoped>
+.meal-card {
+  max-width: 40rem;
+  margin: 2rem auto;
+  padding: 1rem;
+  border: 0.2rem solid black;
+}
+
+</style>
