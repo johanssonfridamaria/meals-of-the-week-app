e33dfcb1b2ccb60ee338b111fa1bbae0779f4e1a
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-21 15:44:54 +0100

    imported mapgetters and added meals from module. created card for each meal

--- src/views/Meals.vue
+++ src/views/Meals.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
+    Meals
+    <div class="meal-card" v-for="(meal, index) in meals" :key="index">
+      <h1>{{ meal.index }}</h1>
+      <h1>{{ meal.name }}</h1>
+      <button class="btn">Remove</button>
+      <button class="btn">Add to "This weeks menu"</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'Meals',
-  components: {
-  }
-}
+  // components: {},
+  // methods: {},
+  computed: {
+    ...mapGetters(['meals']),
+  },
+  // created(){
+
+  // }
+};
 </script>
