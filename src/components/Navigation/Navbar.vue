720e5ae608b7e767481e95a4423e0ab31d91e725
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-21 10:20:33 +0100

    added links to routes

---
+++ src/components/Navigation/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <div>
+      <router-link to="/">Meals</router-link>
+      <router-link to="/menu">This weeks menu</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<style>
+
+</style>
