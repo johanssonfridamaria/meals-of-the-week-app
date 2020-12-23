9b7d6d38035ff15f8c77274cb9ef08accd1d54fa
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-23 15:40:06 +0100

    added symbols to btns and added styling

--- src/components/Meals/MealCard.vue
+++ src/components/Meals/MealCard.vue
@@ -1,22 +1,26 @@
 <template>
-    <div class="meal-card d-flex column">
+    <div class="meal-card d-flex column" :class="{dark: darkTheme }">
       <h1>{{ meal.name }}</h1>
       <div>
-      <button class="btn" @click="delMeal(meal)">Remove</button>
-      <button class="btn" @click="addToMenu(meal)">Add</button>
+      <button class="btn round-btn d-flex" @click="delMeal(meal)"><i class="fas fa-minus"></i></button>
+      <button v-if="meal.menu" class="btn round-btn d-flex" @click="addToMenu(meal)"><i class="fas fa-plus"></i></button>
+      <button v-else class="btn round-btn d-flex" @click="addToMenu(meal)"><i class="fas fa-check"></i></button>
       </div>
     </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'MealCard',
   props: ['meal', 'id'],
+  computed: {
+    ...mapGetters(['darkTheme'])
+  },
   methods: {
     ...mapActions(['delMeal', 'addToMenu'])
-  }
+  },
 };
 </script>
 
@@ -26,6 +30,17 @@ export default {
   margin: 2rem auto;
   padding: 1rem;
   border: 0.2rem solid black;
+  border-radius: 1rem;
 }
+.round-btn{
+  width: 2.5rem;
+  height: 2.5rem;
+  font-size: 1.3rem;
+  padding: 0;
+  border-radius: 50%;
+  align-items: center;
+  justify-content: center;
+  margin: 1rem;
+  }
 
 </style>
