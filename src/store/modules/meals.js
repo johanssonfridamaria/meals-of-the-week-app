6cae21cbf7d4e7dd1a7db1ccfbcbc61e4f9b59d4
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-23 09:02:30 +0100

    added actions and mutation for adding and deleting

--- src/store/modules/meals.js
+++ src/store/modules/meals.js
@@ -1,20 +1,39 @@
 export default {
   state: {
     meals: [
-      { name: 'PalakPaneer' },
+      { name: 'PalakPaneer',},
       { name: 'Pasta' },
       { name: 'Pizza' }
     ],
     thisWeeksMenu: [],
+    randomMeal: ''
   },
   getters: {
     meals: state => state.meals,
-    thisWeeksMenu: state => state.thisWeeksMenu
+    thisWeeksMenu: state => state.thisWeeksMenu,
+    randomMeal: state => state.meals[Math.floor(Math.random() * state.meals.length)]
   },
   mutations: {
-
+    ADD_MEAL: (state, meal) => {
+      state.meals.push({name:meal})
+    },
+    DEL_MEAL: (state, meal) => {
+      state.meals.splice(state.meals.indexOf(meal), 1)
+    },
+    ADD_TO_MENU: (state, meal) =>{
+      state.thisWeeksMenu.push(meal)
+    }
   },
   actions: {
-
-  },
+    // addRanToList: ({commit}) 
+    addMeal: ({commit}, meal) =>{
+      commit('ADD_MEAL', meal)
+    },
+    delMeal: ({commit}, meal) => {
+      commit('DEL_MEAL', meal)
+    },
+    addToMenu: ({commit}, meal) => {
+      commit('ADD_TO_MENU', meal)
+    }
+  }
 }
