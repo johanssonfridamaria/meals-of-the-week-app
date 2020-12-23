36463fb93700ff287a9ad4206dbc07d2d45b5dd1
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-23 09:11:43 +0100

    added mealcard list

--- src/views/ThisWeeksMenu.vue
+++ src/views/ThisWeeksMenu.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>Menu</div>
+  <div>
+    <h1>This weeks menu</h1>
+    <div class="d-flex row-wrap">
+      <mealCard v-for="(meal, index) in thisWeeksMenu" :key="index" :meal="meal" :id="index" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import MealCard from '../components/Meals/MealCard';
+import { mapGetters } from 'vuex';
 
-}
+export default {
+  name: 'ThisWeeksMenu',
+  components: {
+    MealCard,
+  },
+  // methods: {
+  //   ...mapActions(['randomMeal'])
+  // },
+  computed: {
+    ...mapGetters(['thisWeeksMenu', 'randomMeal']),
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
 </style>
