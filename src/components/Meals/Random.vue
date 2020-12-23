205638a1e2a563d65ae18b25f5c7af70c6b6e9e1
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-23 09:02:10 +0100

    created random component

---
+++ src/components/Meals/Random.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h4>
+      Difficult to choose? Click on this button to get a random meal added to
+      this weeks menu
+    </h4>
+    <button class="btn" @click="randomMeal()">Choose for me!</button>
+  </div>
+</template>
+    
+<script>
+export default {
+  name: 'Random',
+};
+</script>
+    
+<style>
+</style>
