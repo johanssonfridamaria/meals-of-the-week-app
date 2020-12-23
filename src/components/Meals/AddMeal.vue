f0a275a1ac88dc1042a3542f2ffded8f7b48339c
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-23 09:01:14 +0100

    added form and add-method

--- src/components/Meals/AddMeal.vue
+++ src/components/Meals/AddMeal.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
-    
+    <label for="name">Name on meal</label>
+    <input type="text" id="name" v-model="name" />
+    <button @click="add">Add</button>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
-
-}
+  data() {
+    return {
+      name: '',
+    };
+  },
+  methods: {
+    ...mapActions(['addMeal']),
+    add() {
+      if (this.name != '') {
+        this.addMeal(this.name);
+        this.name = '';
+      }
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
