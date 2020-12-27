29c334f0129858322f8a98e215627261f5062f82
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-27 20:25:19 +0100

    added dark theme

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <navbar />
-    <div class="container">
+    <div :class="{ dark: darkTheme }" >
       <router-view />
     </div>
     <appFooter />
@@ -11,6 +11,7 @@
 <script>
 import Navbar from './components/Navigation/Navbar';
 import AppFooter from './components/Navigation/AppFooter';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'App',
@@ -18,53 +19,86 @@ export default {
     Navbar,
     AppFooter,
   },
+  methods:{
+    ...mapActions(['getMeals'])
+  },
+    computed: {
+    ...mapGetters(['darkTheme']),
+  },
+  created() {
+    this.getMeals()
+  },
 };
 </script>
 
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
-body, html{
+body,
+html {
   height: 100%;
-  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande',
+    'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 }
-.container{
+.container {
   width: 80%;
   margin: auto;
 }
-.d-flex{
+.d-flex {
   display: flex;
 }
-.column{
+.column {
   flex-direction: column;
 }
-.row{
+.row {
   flex-direction: row;
 }
-.row-wrap{
+.row-wrap {
   flex-flow: row wrap;
 }
-h1{
+h1 {
   font-family: 'Amatic SC', cursive;
   font-size: 2.5rem;
-  margin-bottom: 1rem;
 }
-.btn{
+.btn {
   background: black;
   border: 0.1rem solid transparent;
-  border-radius: 0.25rem;
+  border-radius: 0.5rem;
   padding: 0.5rem 1rem;
   font-size: 1rem;
   outline: none;
   cursor: pointer;
-  color: white;
+  color: #fff;
 }
-.dark{
-  background: black;
-  color: white;
+.dark {
+  background: #003559;
+  /* background: #002C4C; */
+  color: #fff;
 }
-
+.text-center{
+  text-align: center;
+}
+.mb-1{
+  margin-bottom: 1rem;
+}
+.mt-1{
+  margin-top: 1rem;
+}
+.my-1{
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+.mx-1{
+  margin-left: 1rem;
+  margin-right: 1rem;
+}
+.pb-1{
+  padding-bottom: 1rem;
+}
+.pt-1{
+  padding-top: 1rem;
+}
 </style>
