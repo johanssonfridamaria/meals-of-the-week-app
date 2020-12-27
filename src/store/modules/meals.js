8be59cf0b2eb352fd7762cd426bc4be4576b9465
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-27 20:27:01 +0100

    added set meals mutations o get meals from localstorage

--- src/store/modules/meals.js
+++ src/store/modules/meals.js
@@ -14,11 +14,18 @@ export default {
     randomMeal: state => state.meals[Math.floor(Math.random() * state.meals.length)]
   },
   mutations: {
+    SET_MEALS: (state) => {
+      if(localStorage.getItem('meals')){
+        state.meals = JSON.parse(localStorage.getItem('meals'));
+      }
+    },
     ADD_MEAL: (state, meal) => {
       state.meals.push({name:meal})
+      localStorage.setItem('meals', JSON.stringify(state.meals))
     },
     DEL_MEAL: (state, meal) => {
       state.meals.splice(state.meals.indexOf(meal), 1)
+      localStorage.setItem('meals', JSON.stringify(state.meals))
     },
     ADD_TO_MENU: (state, meal) =>{
       state.thisWeeksMenu.push(meal)
@@ -26,6 +33,9 @@ export default {
   },
   actions: {
     // addRanToList: ({commit}) 
+    getMeals: ({commit}) => {
+      commit('SET_MEALS')
+    },
     addMeal: ({commit}, meal) =>{
       commit('ADD_MEAL', meal)
     },
