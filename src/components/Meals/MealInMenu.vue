a31589e01406ca4455f34a3c0ad53fa1704667d9
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-28 14:32:10 +0100

    added component to show meals in menu

---
+++ src/components/Meals/MealInMenu.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="mx-1 meal-card">
+    <div class=" d-flex row meal-content">
+      <h1 class="text-center">{{ meal.name }}</h1>
+      <!-- <div> -->
+      <button class="btn round-btn d-flex" @click="delFromMenu(meal)">
+        <i class="fas fa-minus"></i>
+      </button>
+      <!-- </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'MealInMenu',
+  props: ['meal', 'id'],
+  methods: {
+    ...mapActions(['delFromMenu']),
+  },
+};
+</script>
+
+<style scoped>
+.meal-card {
+  max-width: 40rem;
+  padding: 1rem;
+  border: 0.1rem solid transparent;
+  border-radius: 1rem;
+  background: #f9f9f9;
+  color: #111;
+  align-items: center;
+}
+.round-btn {
+  width: 2.5rem;
+  height: 2.5rem;
+  font-size: 1.3rem;
+  padding: 0;
+  border-radius: 50%;
+  align-items: center;
+  justify-content: center;
+  margin-left: 1rem;
+}
+.meal-content{
+  align-items: center;
+}
+</style>
