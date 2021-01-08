32b8ea81721df9cc71bd7f24732b520243a0ef18
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-08 11:23:01 +0100

    added id to the meals and changed the muatations accordingly

--- src/store/modules/meals.js
+++ src/store/modules/meals.js
@@ -21,20 +21,20 @@ export default {
         state.meals = JSON.parse(localStorage.getItem('meals'));
       }
     },
-    ADD_MEAL: (state, meal) => {
-      state.meals.push({id: meal.id, name:meal.name})
+    ADD_MEAL: (state, mealName) => {
+      state.meals.push({id: Date.now(), name:mealName})
       localStorage.setItem('meals', JSON.stringify(state.meals))
     },
-    DEL_MEAL: (state, meal) => {
-      state.meals.splice(state.meals.indexOf(meal), 1)
+    DEL_MEAL: (state, id) => {
+      state.meals = state.meals.filter(meal => meal.id !== id)
       localStorage.setItem('meals', JSON.stringify(state.meals))
     },
     ADD_TO_MENU: (state, meal) =>{
       state.menu.push(meal)
       localStorage.setItem('menu', JSON.stringify(state.menu))
     },
-    DEL_FROM_MENU: (state, meal) => {
-      state.menu.splice(state.menu.indexOf(meal), 1)
+    DEL_FROM_MENU: (state, id) => {
+      state.menu = state.menu.filter(meal => meal.id !== id)
       localStorage.setItem('menu', JSON.stringify(state.menu))
     },
     
@@ -44,17 +44,17 @@ export default {
     getMeals: ({commit}) => {
       commit('SET_MEALS')
     },
-    addMeal: ({commit}, meal) =>{
-      commit('ADD_MEAL', meal)
+    addMeal: ({commit}, mealName) =>{
+      commit('ADD_MEAL', mealName)
     },
-    delMeal: ({commit}, meal) => {
-      commit('DEL_MEAL', meal)
+    delMeal: ({commit}, id) => {
+      commit('DEL_MEAL', id)
     },
     addToMenu: ({commit}, meal) => {
       commit('ADD_TO_MENU', meal)
     },
-    delFromMenu: ({commit}, meal) => {
-      commit('DEL_FROM_MENU', meal)
+    delFromMenu: ({commit}, id) => {
+      commit('DEL_FROM_MENU', id)
     }
   }
 }
