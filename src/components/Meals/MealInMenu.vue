9d176b7a7e94a23a47159118ff81cdbc64507ba5
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-11 22:45:30 +0100

    updated

--- src/components/Meals/MealInMenu.vue
+++ src/components/Meals/MealInMenu.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="mx-1 meal-card">
-    <div class=" d-flex row meal-content">
-      <h1 class="text-center">{{ meal.name }}</h1>
-      <button class="btn round-btn d-flex" @click="delFromMenu(meal)">
+  <div class="d-flex meal">
+    <h2>{{ meal.name }}</h2>
+    <div>
+      <button class="btn round-btn d-flex" @click="delFromMenu(meal.id)">
         <i class="fas fa-minus"></i>
       </button>
     </div>
@@ -22,16 +22,11 @@ export default {
 </script>
 
 <style scoped>
-.meal-card {
-  max-width: 40rem;
-  padding: 1rem;
-  border: 0.1rem solid transparent;
-  border-radius: 1rem;
-  background: #f9f9f9;
-  color: #111;
+.meal {
+  width: 100%;
+  justify-content: space-between;
   align-items: center;
-}
-.meal-content{
-  align-items: center;
+  color:black;
+  margin: 0 0.7rem;
 }
 </style>
