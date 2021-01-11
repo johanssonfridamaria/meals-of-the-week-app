57bd6a5974769ebdb5d73386d2e0f0853e6ccc9d
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-11 22:47:02 +0100

    added menu container, updated css

--- src/views/ThisWeeksMenu.vue
+++ src/views/ThisWeeksMenu.vue
@@ -1,28 +1,39 @@
 <template>
-  <div>
-    <h1>This weeks menu</h1>
-    <div class="d-flex row-wrap">
-      <div v-if="!menu">
-        <h2>You have not added any meals yet</h2>
+  <div class="d-flex menu-container column">
+    <div class="mb-1 card d-flex column"> 
+       <h3 >Go to <router-link class="link" :class="{'text-dark': isDarkTheme }" to="/meals">
+       Meals
+      </router-link> to find more meals to add</h3>
+      <random-meal />
+    </div>
+
+
+    <div class="d-flex column menu">
+      <div class="menu-row ">
+        <h1 class="mb-1 text-center">This weeks menu</h1>
       </div>
-      <mealCard v-else
-        v-for="meal in menu"
-        :key="meal.id"
-        :meal="meal"
-        @minusClick="delFromMenu(meal.id)"
-      />
+      <div class="menu-row" v-for="meal in menu" :key="meal.id">
+        <div v-if="!menu">
+          <h2>You have not added any meals yet</h2>
+        </div>
+        <mealInMenu v-else :meal="meal" @minusClick="delFromMenu(meal.id)" />
+      </div>
+      <div class="menu-row"></div>
+      <div class="menu-row"></div>
     </div>
   </div>
 </template>
 
 <script>
-import MealCard from '../components/Meals/MealCard';
 import { mapGetters, mapActions } from 'vuex';
+import MealInMenu from '../components/Meals/MealInMenu.vue';
+import RandomMeal from '../components/Meals/RandomMeal.vue';
 
 export default {
   name: 'ThisWeeksMenu',
   components: {
-    MealCard,
+    MealInMenu,
+    RandomMeal,
   },
   methods: {
     ...mapActions(['delFromMenu']),
@@ -34,4 +45,35 @@ export default {
 </script>
 
 <style scoped>
+.card{
+align-items: center;
+}
+.link{
+  text-decoration: none;
+  color:  #53785a;
+  font-size: 1rem; 
+  cursor: pointer;
+}
+.menu-container{
+  align-items: center;
+}
+.menu {
+  background: #fff;
+  border-bottom: 0.2rem solid #000;
+  border-radius: 0.5rem;
+  padding: 2rem;
+  color: #000;
+  width: 100%;
+  max-width: 40rem;
+}
+.menu-row {
+  height: 4.5rem;
+  border-bottom: 0.2rem solid #000;
+  display: flex;
+  align-content: center;
+  align-items: center;
+}
+/* .menu-row h1{
+  text-align: center;
+} */
 </style>
