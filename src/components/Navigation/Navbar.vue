e832ee57a0bb23e409995ee2c767137534619fc6
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-11 22:45:58 +0100

    changed dark theme styling

--- src/components/Navigation/Navbar.vue
+++ src/components/Navigation/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
-  <nav class="navbar d-flex column" :class="{ navbarDark:isDarkTheme }">
-    <h1>What shall we eat this week?</h1>
-    <div class="d-flex row my-1">
-      <router-link class="nav-link" :class="{ navbarDark: isDarkTheme }" to="/"
-        >Meals</router-link
-      >
+  <nav class="navbar d-flex column" :class="{'text-dark': isDarkTheme }">
+    <h1 class="text-center">What shall we eat this week?</h1>
+    <div class="d-flex row my-1 ">
+      <router-link class="nav-link" :class="{'text-dark': isDarkTheme }" to="/">
+        This weeks menu
+      </router-link>
       <router-link
-        class="nav-link"
-        :class="{ navbarDark: isDarkTheme }"
-        to="/menu"
-        >This weeks menu</router-link
-      >
+        class="nav-link ml-3"
+        :class="{ 'text-dark': isDarkTheme }"
+        to="/meals"
+        >Meals
+      </router-link>
     </div>
-    <button v-if="!isDarkTheme" class="btn btn-theme" @click="setTheme()">
+    <button v-if="isDarkTheme" class="btn btn-theme" @click="setTheme()">
       <i class="fas fa-sun"></i>
     </button>
     <button v-else class="btn btn-theme" @click="setTheme()">
@@ -27,30 +27,24 @@ import { mapActions, mapGetters } from 'vuex';
 export default {
   name: 'Navbar',
   methods: {
-    ...mapActions(['setTheme'])
+    ...mapActions(['setTheme']),
   },
   computed: {
     ...mapGetters(['isDarkTheme']),
-    
-  }
-
+  },
 };
 </script>
 
 <style scoped>
 .navbar {
   background-color: #53785a;
-  padding: 1rem 0;
+  padding: 1rem 2rem;
   align-items: center;
 }
 .nav-link {
   text-decoration: none;
   color: #333;
-  margin-left: 3rem;
   font-size: 1rem;
-}
-.navbarDark {
-  color: #fff;
 }
 .btn-theme {
   background: transparent;
