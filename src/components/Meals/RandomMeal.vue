0a9b6b285fe81f026b48d1ddb60753e81d7f7614
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-11 22:45:41 +0100

    created random meal component

---
+++ src/components/Meals/RandomMeal.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="random d-flex">
+    <h2 class="">Difficult to choose?</h2>
+    <button class="btn ml-3">Add a random meal!</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.random{
+  align-items: center;
+}
+</style>
