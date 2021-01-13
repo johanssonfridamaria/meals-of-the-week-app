d49e8712639930916fd85b1cd5c6dc0d5c848db3
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-13 09:32:03 +0100

    changed layout of input group for mobile view

--- src/components/Meals/AddMeal.vue
+++ src/components/Meals/AddMeal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex column section">
     <h2 class="text-center mb-1">Don't see anything you like? Add meals:</h2>
-    <div class="input-group">
+    <div class="input-group d-flex column">
       <input
         type="text"
         id="name"
@@ -36,14 +36,33 @@ export default {
 <style scoped>
 input {
   padding: 0.7rem 0.7rem;
-  border-radius: 0.5rem 0 0 0.5rem;
+  border-radius: 0.5rem;
   border: none;
   font-size: 1rem;
   outline: none;
   background: #eee;
   color: #333;
+  margin-bottom: 1rem;
+  width: 100%;
 }
 .btn-add {
-  border-radius: 0 0.5rem 0.5rem 0;
+  border-radius: 0.5rem;
+}
+.input-group {
+  width: 100%;
+  justify-content: center;
+}
+@media (min-width: 592px) {
+  .input-group {
+    flex-direction: row;
+    max-width: 25rem;
+  }
+  input {
+    border-radius: 0.5rem 0 0 0.5rem;
+    margin-bottom: 0;
+  }
+  .btn-add {
+    border-radius: 0 0.5rem 0.5rem 0;
+  }
 }
 </style>
